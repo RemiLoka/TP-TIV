a5e3526f204223ecee232df11d5d600f6b0e8b00
RemiLoka <remi.lokajczyk@ensea.fr>
2023-11-09 11:10:36 +0100

    Histogram

--- 1-Rémi.py
+++ 1-Rémi.py
@@ -1,5 +1,8 @@
 import cv2
 import os
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from PIL import Image
 
 def extract_frame(video_path):
     video_capture = cv2.VideoCapture(video_path)
@@ -32,6 +35,34 @@ def initialize_tracking(video_capture):
   video_capture.release()
   return roi
 
-roi = initialize_tracking(video_path)
+roi = [300,218,42,42]
 
-print(roi)
+def show_square(roi,frame):
+    im = Image.open(frame)
+    fig, ax = plt.subplots()
+    ax.imshow(im)
+    rect = patches.Rectangle((roi[0], roi[1]), roi[2], roi[3], linewidth=1, edgecolor='r', facecolor='none')
+
+    ax.add_patch(rect)
+
+    plt.show()
+
+show_square(roi,'frames/frame_0.jpg')
+
+def histo_roi(roi, frame):
+    image = cv2.imread(frame)
+    initial_area = (roi[0], roi[1], roi[0]+roi[2], roi[1]+roi[3])
+
+    tracked_area = image[roi[1]:roi[1]+roi[3]+20, roi[0]:roi[0]+roi[2]+20]
+    hsv = cv2.cvtColor(tracked_area, cv2.COLOR_BGR2HSV)
+    hist = cv2.calcHist([hsv], [0], None, [180], [0, 180])
+    cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX)
+
+    plt.plot(hist)
+    plt.title('Histogram of the Tracked Area')
+    plt.xlabel('Pixel Value')
+    plt.ylabel('Frequency')
+    plt.show()
+    return hist
+
+histo_roi(roi,'frames/frame_0.jpg')
