960731e7ca73cecd1c60ebe306ce46c30068c42d
RemiLoka <remi.lokajczyk@ensea.fr>
2023-11-15 11:39:31 +0100

    Cleaning

--- Amine.py
+++ Amine.py
@@ -1,104 +1,102 @@
+########## Import ##########
+
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+########## Data ##########
+
+
 cap = cv2.VideoCapture('data/synthetic/escrime-4-3.avi')
 
-#cap = cv2.VideoCapture('data/synthetic/escrime-4-3-cluster.avi')
 
-def calculate_histogram(image, tracked_area):
+########## Function ##########
+
+
+def calculate_histogramHSV(image, tracked_area):
     x, y, w, h = tracked_area
 
     roi = image[y:y+h, x:x+w]
     hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
 
-    # Using Hue, Saturation, and Value channels
-    channels = [0, 1, 2]  # Hue, Saturation, and Value channels
-    hist_size = [30, 32, 32]  # Number of bins for each channel
-    h_ranges = [0, 180]  # Hue range
-    s_ranges = [0, 256]  # Saturation range
-    v_ranges = [0, 256]  # Value range
+    channels = [0, 1, 2]
+    hist_size = [30, 32, 32]
+    h_ranges = [0, 180]
+    s_ranges = [0, 256]
+    v_ranges = [0, 256]
 
     roi_hist = cv2.calcHist([hsv_roi], channels, None, hist_size, h_ranges + s_ranges + v_ranges)
     cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
     return roi_hist.flatten()
 
 
-
-def calculate_histogram(image, tracked_area):
+def calculate_histogramRGB(image, tracked_area):
     x, y, w, h = tracked_area
     roi = image[y:y+h, x:x+w]
 
-    # Using RGB channels
-    channels = [0, 1, 2]  # Red, Green, and Blue channels
-    hist_size = [32, 32, 32]  # Number of bins for each channel
-    ranges = [0, 256, 0, 256, 0, 256]  # Ranges for RGB channels
+    channels = [0, 1, 2]
+    hist_size = [32, 32, 32]
+    ranges = [0, 256, 0, 256, 0, 256]
 
     roi_hist = cv2.calcHist([roi], channels, None, hist_size, ranges)
     cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
     return roi_hist.flatten()
 
 
+def initialize_tracking(video_capture,type = 'RGB'):
+    _,frame=video_capture.read()
 
-
-
-def initialize_tracking(video_capture):
-  _,frame=video_capture.read()
-
-  roi = cv2.selectROI("Select Object", frame, fromCenter=False, showCrosshair=True)
-  cv2.destroyWindow("Select Object")
-
-  roi_hist = calculate_histogram(frame, roi)
-  return roi_hist, roi
-
+    roi = cv2.selectROI("Select Object", frame, fromCenter=False, showCrosshair=True)
+    cv2.destroyWindow("Select Object")
+    if type == 'RGB':
+        roi_hist = calculate_histogramRGB(frame, roi)
+    elif type == 'HSV':
+        roi_hist = calculate_histogramHSV(frame, roi)
+    return roi_hist, roi
 
 
 def initialize_particles(roi, num_particles):
-  x, y, w, h = roi
-  center=(x+w//2, y+h//2)
+    x, y, w, h = roi
+    center=(x+w//2, y+h//2)
 
-  particles=np.array([np.random.normal(center, 1) for _ in range(num_particles)])
-  weights=np.ones(num_particles)/num_particles
+    particles=np.array([np.random.normal(center, 1) for _ in range(num_particles)])
+    weights=np.ones(num_particles)/num_particles
 
-  return particles, weights
-
+    return particles, weights
 
 
 def predict_particles(particles,sigma):
-  noise = np.random.randn(*particles.shape) * sigma
-  particles += noise
-  return particles
-   
+    noise = np.random.randn(*particles.shape) * sigma
+    particles += noise
+    return particles
 
 
+def weights_update(particles,frame,hist_ref,roi_size, type = 'RGB', lamda=3):
+    weights=np.zeros(particles.shape[0])
 
+    for i, particle in enumerate(particles):
+        x, y = particle
+        w,h = roi_size
+        roi= (int(x-w//2), int(y-h//2), w, h)
 
+        if type == 'RGB':
+            roi_hist=calculate_histogramRGB(frame,roi)
+        elif type == 'HSV':
+            roi_hist=calculate_histogramHSV(frame,roi)
 
+        distance=cv2.compareHist(hist_ref,roi_hist,cv2.HISTCMP_BHATTACHARYYA)
+        weights[i]=np.exp(-lamda*(distance**2))
+    weights /= np.sum(weights)
 
-def weights_update(particles,frame,hist_ref,roi_size,lamda=3):
-  weights=np.zeros(particles.shape[0])
-
-  for i, particle in enumerate(particles):
-   x, y = particle
-   w,h = roi_size
-   roi= (int(x-w//2), int(y-h//2), w, h)
-
-   roi_hist=calculate_histogram(frame,roi)
-   #distance = histogram_distance(hist_ref, roi_hist)
-   distance=cv2.compareHist(hist_ref,roi_hist,cv2.HISTCMP_BHATTACHARYYA)
-
-   weights[i]=np.exp(-lamda*(distance**2))
-  weights /= np.sum(weights)
-
-  return weights
+    return weights
 
 
 def resample(particles, weights):
     N = len(particles)
     cumulative_sum = np.cumsum(weights)
 
-    # Use systematic resampling to select particles based on their weights
     indexes = np.zeros(N, dtype=int)
     i, j = 0, 0
     u = np.random.rand() / N
@@ -109,19 +107,11 @@ def resample(particles, weights):
         indexes[i] = j
 
     resampled_particles = particles[indexes]
-    uniform_weights = np.ones(N) / N
-
-    return resampled_particles, uniform_weights
-
-
+    return resampled_particles
 
 
 def estimate_position(particles, weights):
     return np.average(particles, weights=weights, axis=0)
-
-
-
-
 
 
 def visualize_tracking(frame, particles, pos_estimate, roi_size):
@@ -135,19 +125,25 @@ def visualize_tracking(frame, particles, pos_estimate, roi_size):
     # Draw each particle as a small dot
     for particle in particles:
         px, py = int(particle[0]), int(particle[1])
-        cv2.circle(frame, (px, py), 1, (0, 0, 255), -1)  # Red dot for each particle
+        cv2.circle(frame, (px, py), 1, (0, 0, 255), -1)
 
     return frame
 
 
+########## Run ##########
 
 
-roi_hist,roi = initialize_tracking(cap)
-
-#plt.bar(range(len(roi_hist)), roi_hist, width=1)
-#plt.show()
+    ########## Parameters ##########
 
 num_particles = 100
+sigma = np.array([4,4])
+lamda = 3
+type = 'RGB'            # 'RGB' or 'HSV'
+
+
+    ########## While loop ##########
+
+roi_hist,roi = initialize_tracking(cap,type)
 particles, weights = initialize_particles(roi, num_particles)
 
 while True:
@@ -155,14 +151,10 @@ while True:
     if not ret:
         break
     
-    sigma=np.array([4,4])
     particles=predict_particles(particles,sigma)
-
-    weights=weights_update(particles,frame,roi_hist,roi[2:])
-    particles,weights=resample(particles,weights)
-
+    weights=weights_update(particles,frame,roi_hist,roi[2:], type, lamda)
+    particles = resample(particles,weights)
     pos_estimate=estimate_position(particles,weights)
-
     visualize_tracking(frame,particles,pos_estimate,roi[2:])
 
     cv2.imshow('frame', frame)
